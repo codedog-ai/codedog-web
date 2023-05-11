f7db03ee843800614d03df0c90cdf2430f2f93b2
arcadia.han <arcadia.han@cloudwise.com>
2023-05-11 21:42:24 +0800

    init

---
+++ app.config.ts
@@ -0,0 +1,35 @@
+export default defineAppConfig({
+    docus: {
+        title: 'Codedog',
+        description: 'Your AI code review partner.',
+        image: '',
+        url: 'https://www.codedog.ai',
+        layout: "default",
+        socials: {
+        },
+        aside: {
+            level: 0,
+            exclude: [],
+            collapsed: false
+        },
+        header: {
+            logo: false,
+            showLinkIcon: false,
+            fluid: false,
+        },
+        footer: {
+            credits: {
+                icon: "IconDocus",
+                text: "Powered by Docus",
+                href: "https://docus.dev"
+            },
+            iconLinks: [
+
+            ],
+            textLinks: [
+
+            ],
+            fluid: false
+        }
+    }
+})
