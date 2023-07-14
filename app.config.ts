042f8af38cb3062a450fecfb37554645ea5c44b4
peter.lin <peter.lin@cloudwise.com>
2023-07-14 11:53:44 +0800

    update app description

--- app.config.ts
+++ app.config.ts
@@ -1,37 +1,35 @@
 export default defineAppConfig({
-    docus: {
-        title: 'Codedog',
-        description: 'Your AI code review partner.',
-        image: '',
-        url: 'https://www.codedog.ai',
-        layout: "default",
-        socials: {
+  docus: {
+    title: "Codedog",
+    description:
+      "Codedog is an AI powered code reivew assistant. Codedog helps you and your team automate the generation of merge request summaries and reviews.",
+    image: "",
+    url: "https://www.codedog.ai",
+    layout: "default",
+    socials: {},
+    aside: {
+      level: 1,
+      exclude: [],
+      collapsed: false,
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+    },
+    footer: {
+      credits: {
+        icon: "IconDocus",
+        text: "Powered by Docus",
+        href: "https://docus.dev",
+      },
+      iconLinks: [],
+      textLinks: [
+        {
+          text: "加入我们",
+          href: "join_us",
         },
-        aside: {
-            level: 1,
-            exclude: [],
-            collapsed: false
-        },
-        header: {
-            logo: false,
-            showLinkIcon: true,
-        },
-        footer: {
-            credits: {
-                icon: "IconDocus",
-                text: "Powered by Docus",
-                href: "https://docus.dev"
-            },
-            iconLinks: [
-
-            ],
-            textLinks: [
-                {
-                    text: "加入我们",
-                    href: "join_us"
-                }
-            ],
-            fluid: false
-        }
-    }
-})
+      ],
+      fluid: false,
+    },
+  },
+});
